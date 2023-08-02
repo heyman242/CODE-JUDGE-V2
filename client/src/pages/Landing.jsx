0b19d67bd77b89ecc27806e232cbb0e1518164c8
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-02 16:04:29 +0530

    frontend landing page setup

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,32 @@
+import Wrapper from "../assets/wrappers/Landing";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Code <span>Judge</span>
+          </h1>
+          <p>
+            welcome to the code judge
+          </p>
+          <Link to="/register" className="btn register-link">
+            register
+          </Link>
+          <Link to="/login" className="btn ">
+            login / Demo User
+          </Link>
+        </div>
+        
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
