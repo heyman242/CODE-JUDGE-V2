e6b87bc469082eb3472eeb67fa20c8d91b121ae0
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-02 17:29:01 +0530

    backend basic folder setup

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -5,8 +5,8 @@ import { ImProfile } from "react-icons/im";
 import { MdAdminPanelSettings } from "react-icons/md";
 
 const links = [
-  { text: "add job", path: ".", icon: <FaWpforms /> },
-  { text: "all jobs", path: "all-jobs", icon: <MdQueryStats /> },
+  { text: "All Questions", path: ".", icon: <FaWpforms /> },
+  { text: "Add Question", path: "all-jobs", icon: <MdQueryStats /> },
   { text: "stats", path: "stats", icon: <IoBarChartSharp /> },
   { text: "profile", path: "profile", icon: <ImProfile /> },
   { text: "admin", path: "admin", icon: <MdAdminPanelSettings /> },
