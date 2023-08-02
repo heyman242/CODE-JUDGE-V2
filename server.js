26d300b4179b0a96b63f4b3787fecae470897a4e
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-02 18:38:53 +0530

    backend added user models schema

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+import "express-async-errors";
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
 const app = express();
+
+//ROUTERS
+
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -17,6 +24,12 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
