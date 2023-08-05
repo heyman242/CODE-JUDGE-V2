4f3a4ca8199f18f5ffe57ea4e4712fa3fd004323
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-05 18:05:36 +0530

    frontend updated login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,33 @@
-import { Logo, FormRow } from "../components";
+import { Logo, FormRow, SubmitBtn } from "../components";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+import { Link, Form, redirect } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login Successful");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
-        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
-        <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          submit
-        </button>
+        <FormRow type="email" name="email" defaultValue="himanshu.nitraipur21@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="1234567890" />
+        <SubmitBtn formBtn />
         <button type="button" className="btn btn-block">
           explore the app
         </button>
@@ -23,7 +37,7 @@ const Login = () => {
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
